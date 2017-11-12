bfe270ff99a9076cdfe83c96f21efb01c3443390
brianhuang1019 <brianhuang1019@gmail.com>
2017-11-12 19:22:10 +0800

    support evaluation by class or its instance

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,6 +6,12 @@ from scipy import spatial
 import numpy as np
 
 
+class Evaluation(object):
+
+  def make_samples(self):
+    raise NotImplementedError("Needs to implemented this method")
+
+
 def distance(v1, v2, d_type='d1'):
   assert v1.shape == v2.shape, "shape of two vectors need to be same!"
 
@@ -27,6 +33,8 @@ def distance(v1, v2, d_type='d1'):
     return 2 - 2 * np.dot(v1, v2)
   elif d_type == 'cosine':
     return spatial.distance.cosine(v1, v2)
+  elif d_type == 'square':
+    return np.sum((v1 - v2) ** 2)
 
 
 def AP(label, results, sort=True):
@@ -58,10 +66,11 @@ def infer(query, samples=None, db=None, sample_db_fn=None, depth=None, d_type='d
   ''' infer a query, return it's ap
 
     arguments
-      query       : a dict with two keys, see the example below
+      query       : a dict with three keys, see the template
                     {
-                      'img': 'orange_1.jpg',
-                      'cls': 'orange'
+                      'img': <path_to_img>,
+                      'cls': <img class>,
+                      'hist' <img histogram>
                     }
       samples     : a list of {
                                 'img': <path_to_img>,
@@ -88,7 +97,7 @@ def infer(query, samples=None, db=None, sample_db_fn=None, depth=None, d_type='d
                     'cls': s_cls
                   })
   results = sorted(results, key=lambda x: x['dis'])
-  if depth:
+  if depth and depth <= len(results):
     results = results[:depth]
   ap = AP(q_cls, results, sort=False)
 
@@ -108,6 +117,32 @@ def evaluate(db, sample_db_fn, depth=None, d_type='d1'):
   ret = {c: [] for c in classes}
 
   samples = sample_db_fn(db)
+  for query in samples:
+    ap, _ = infer(query, samples=samples, depth=depth, d_type=d_type)
+    ret[query['cls']].append(ap)
+
+  return ret
+
+
+def evaluate_class(db, f_class=None, f_instance=None, depth=None, d_type='d1'):
+  ''' infer the whole database
+
+    arguments
+      db     : an instance of class Database
+      f_class: a class that generate features, needs to implement make_samples method
+      depth  : retrieved depth during inference, the default depth is equal to database size
+      d_type : distance type
+  '''
+  assert f_class or f_instance, "needs to give class_name or an instance of class"
+
+  classes = db.get_class()
+  ret = {c: [] for c in classes}
+
+  if f_class:
+    f = f_class()
+  elif f_instance:
+    f = f_instance
+  samples = f.make_samples(db)
   for query in samples:
     ap, _ = infer(query, samples=samples, depth=depth, d_type=d_type)
     ret[query['cls']].append(ap)
